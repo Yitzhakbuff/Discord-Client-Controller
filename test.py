c5a96df7ecddba8f4eb7601a317fb30b058520ad
Codey <codeybyte@gmail.com>
2024-02-26 21:44:20 -0300

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,19 @@
 import requests
-import time
-while True:
-    r=requests.post("https://discord.com/api/v9/guilds/1211384349107167372/roles", headers={"Authorization":"-I.zI3IqQjRTnoGKrdGrgOHMNfdrxXboFew5sQPAc"}, data={"name":"pene", "permissions":"0", "color":"0"})
-    time.sleep(0.5)
-print(r.status_code)
+import datetime
+
+def get_user_info(user_id):
+    response = requests.get(
+        f"https://discord.com/api/v9/users/{user_id}",
+        headers={"Authorization": "MTIxMTQ2ODk2MTYyMzk2NTcyNw.GAwfmt.MvHoAKVPRlLtufbpMVo6QyN8lp2zWE-OanW2Ws"}
+    )
+    user_data = response.json()
+    xd = "✅" if user_data['premium_type'] == 2 else "❌"
+    timestamp = int(user_id) // 4194304 + 1420070400000
+    creation_date = datetime.datetime.fromtimestamp(timestamp / 1000)
+    print(f"\033[1;34mName:\033[0m {user_data['global_name']}")
+    print(f"\033[1;34mUsername:\033[0m {user_data['username']}")
+    print(f"\033[1;34mAvatar:\033[0m https://cdn.discordapp.com/avatars/{user_id}/{user_data['avatar']}.png?size=2048")
+    print(f"\033[1;34mCreation Date:\033[0m {creation_date}")
+    print(f"\033[1;34mNitro:\033[0m {xd}")
+user_id = input("\033[1;32mColoca el id del usuario:\033[0m ")
+get_user_info(user_id)
